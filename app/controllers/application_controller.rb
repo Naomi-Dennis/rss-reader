92d7b08fa025cf7340599682cf2fb35dde5b9147
Naomi Dennis <njdennis94@gmail.com>
2018-02-18 16:12:08 +0000

    Navigation Bar functions for loggedin and out users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,26 +6,32 @@ class ApplicationController < Sinatra::Base
   # use Rake::Flash
 
   get '/' do
+    @current_user = Utility.getLoggedUser
     erb :index
   end
 
   get '/account' do
+    @current_user = Utility.getLoggedUser
     erb :account
   end
 
   get '/forgot_pass' do
+    @current_user = Utility.getLoggedUser
     erb :forgot_pass
   end
 
   get '/login' do
+    @current_user = Utility.getLoggedUser
     erb :login
   end
 
   get '/signup' do
+    @current_user = Utility.getLoggedUser
     erb :signup
   end
 
   get '/feeds' do
+    @current_user = Utility.getLoggedUser
     erb :view_feeds
   end
 
