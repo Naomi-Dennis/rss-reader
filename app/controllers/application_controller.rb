f08f0969559228122a9da7bc96e4555ddbd2864c
Naomi Dennis <njdennis94@gmail.com>
2018-02-18 16:36:51 +0000

    Nav Bar Updated. Bug Fixed -- Utility Class Removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,32 +6,34 @@ class ApplicationController < Sinatra::Base
   # use Rake::Flash
 
   get '/' do
-    @current_user = Utility.getLoggedUser
+
+    @current_user = User.getLoggedUser(session[:id])
+      binding.pry
     erb :index
   end
 
   get '/account' do
-    @current_user = Utility.getLoggedUser
+    @current_user = User.getLoggedUser(session[:id])
     erb :account
   end
 
   get '/forgot_pass' do
-    @current_user = Utility.getLoggedUser
+  @current_user = User.getLoggedUser(session[:id])
     erb :forgot_pass
   end
 
   get '/login' do
-    @current_user = Utility.getLoggedUser
+  @current_user = User.getLoggedUser(session[:id])
     erb :login
   end
 
   get '/signup' do
-    @current_user = Utility.getLoggedUser
+    @current_user = User.getLoggedUser(session[:id])
     erb :signup
   end
 
   get '/feeds' do
-    @current_user = Utility.getLoggedUser
+  @current_user = User.getLoggedUser(session[:id])
     erb :view_feeds
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
-class User < ActiveRecord::Base 
-  
-end 
+class User < ActiveRecord::Base
+  def self.isLoggedIn?(session)
+    !session.nil?
+  end
+
+  def self.getLoggedUser(session)
+    if isLoggedIn?(session)
+      User.find_by(id: session[:id])
+    else
+      nil
+    end
+  end
+end
