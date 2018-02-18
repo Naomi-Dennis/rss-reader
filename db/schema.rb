272c376126a889626c49caef6a948bdcdfb168a3
Naomi Dennis <njdennis94@gmail.com>
2018-02-18 19:16:35 +0000

    Feed Schema Updated to include name of Feed

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213162010) do
+ActiveRecord::Schema.define(version: 20180218191242) do
 
   create_table "feed", force: :cascade do |t|
     t.string "url"
     t.integer "user_id"
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
